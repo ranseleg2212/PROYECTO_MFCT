6c1eca943fc3b8f67ebb3f63677bc2cf346f75cf
ranseleg2212 <2190009@ipisa.edu.do>
2022-11-16 22:49:24 -0400

    Corección de error con el despliegue del nav-bar

--- form1.php
+++ form1.php
@@ -419,7 +419,8 @@
 
   </footer>
   <!--Fin Footer-->
-
+  <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
