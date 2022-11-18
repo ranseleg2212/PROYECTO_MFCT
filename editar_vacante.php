7d0d3adc25ef1fa88c6e320508d533cc4ada8b4a
Clara2025 <115728348+Clara2025@users.noreply.github.com>
2022-11-18 07:57:34 -0400

    Detalles vacante

--- editar_vacante.php
+++ editar_vacante.php
@@ -13,7 +13,7 @@ $row = mysqli_fetch_assoc($data);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Do</title>
+    <title>Dom</title>
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
--- form3.php
+++ form3.php
@@ -6,7 +6,7 @@
   <link rel="icon" type="image/png" href="im/logo.png">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Vacantes</title>
+  <title>Vacantes-</title>
   <link rel="stylesheet" href="css/form3.css">
   <!-- CSS only -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
